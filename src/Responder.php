3f536b96a1cbe38d6313efaef8438918328cd6ed
mitoop <me@mitoop.com>
2025-05-02 21:18:45 +0800

    adjust Responder
    Signed-off-by: mitoop <me@mitoop.com>

--- src/Responder.php
+++ src/Responder.php
@@ -3,22 +3,23 @@
 namespace Mitoop\Http;
 
 use Illuminate\Http\JsonResponse;
+use stdClass;
 
 class Responder
 {
     public function __construct(protected ResponseGenerator $generator, protected Config $config) {}
 
-    public function success($data, $message): JsonResponse
+    public function success($data = new stdClass, $message = 'ok'): JsonResponse
     {
         return $this->generator->generate($data, $message, $this->config->success());
     }
 
-    public function error($message, $code, $data): JsonResponse
+    public function error($message = 'error', ?int $code = null, $data = new stdClass): JsonResponse
     {
         return $this->generator->generate($data, $message, $code ?? $this->config->error());
     }
 
-    public function reject($message, $data): JsonResponse
+    public function reject($message = 'failed', $data = new stdClass): JsonResponse
     {
         return $this->generator->generate($data, $message, $this->config->reject());
     }
